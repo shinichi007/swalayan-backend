778a8fae3b754e0568bf95f397169fe38b18ae05
Al Amin <007.alamin@gmail.com>
2023-02-16 11:48:38 +0700

    update register API

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -27,7 +27,7 @@ class AuthController extends Controller
     {
         $validator = Validator::make($request->all(),[
             'name' => 'required|string|max:255',
-            'phone' => 'required|string|max:15',
+            'phone' => 'required|string|min:8|max:18|unique:users',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6',
             'password_confirmation' => 'required_with:password|same:password|min:6'
@@ -39,6 +39,7 @@ class AuthController extends Controller
 
         $user = User::create([
             'name' => $request->name,
+            'phone' => $request->phone,
             'email' => $request->email,
             'password' => Hash::make($request->password)
          ]);
